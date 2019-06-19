57fe038b76865fbe2717a113642f9a0c244688e9
muhamadhafizal <2015105907@isiswa.uitm.edu.my>
2019-06-19 12:52:31 +0800

    bug:index.php

--- web/app/themes/dbpmarketplace/index.php
+++ web/app/themes/dbpmarketplace/index.php
@@ -1,27 +1,27 @@
  
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>DBPMarketplace</title>
+    <title>E-DBP</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/bootstrap.min.css" >
     <!-- Icon -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/fonts/line-icons.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/fonts/line-icons.css">
     <!-- Owl carousel -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/owl.theme.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/owl.theme.css">
     
     <!-- Animate -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/animate.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/animate.css">
     <!-- Main Style -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/main.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/main.css">
     <!-- Responsive Style -->
-    <link rel="stylesheet" href="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/css/responsive.css">
+    <link rel="stylesheet" href="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/css/responsive.css">
 
   </head>
   <body>
@@ -32,7 +32,7 @@
       <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
         <div class="container">
           <!-- Brand and toggle get grouped for better mobile display -->
-          <a href="index.html"><img src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/img/dbpLogo.gif" height="40px" width="40px" alt=""></a>       
+          <a href="index.html"><img src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/img/eDBPFinal.png" height="90px" width="90px" alt=""></a>       
           <div class="collapse navbar-collapse" id="navbarCollapse">
       
            
@@ -49,14 +49,14 @@
                 <h2 class="head-title">E-DBP</h2>
                 <h4>Dewan Bahasa Dan Pustaka</h4>
                 <div class="header-button">
-                  <a href="https://play.google.com/store/apps/details?id=com.dbp.marketplace" ><img src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/img/playstore.png" height="70" width="200"></i></a>
-                  <a href="#"><img src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/img/appstore.jpg" height="70" width="200"></i></a>
+                  <a href="https://play.google.com/store/apps/details?id=com.dbp.marketplace" ><img src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/img/playstore.png" height="70" width="200"></i></a>
+                  <a href="#"><img src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/img/appstore.jpg" height="70" width="200"></i></a>
                 </div>
               </div>
             </div>
             <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
               <div class="intro-img">
-                <img class="img-fluid" src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/img/intro-mobile.png" alt="">
+                <img class="img-fluid" src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/img/intro-mobile.png" alt="">
               </div>            
             </div>
           </div> 
@@ -69,7 +69,7 @@
 
     <!-- Go to Top Link -->
     <a href="#" class="back-to-top">
-    	<i class="lni-arrow-up"></i>
+      <i class="lni-arrow-up"></i>
     </a>
     
     <!-- Preloader -->
@@ -79,17 +79,17 @@
     <!-- End Preloader -->
     
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/jquery-min.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/popper.min.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/bootstrap.min.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/owl.carousel.min.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/wow.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/jquery.nav.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/scrolling-nav.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/jquery.easing.min.js"></script>  
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/main.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/form-validator.min.js"></script>
-    <script src="http://localhost:8082/wordpress/DBP/web/app/themes/dbpmarketplace/assets/js/contact-form-script.min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/jquery-min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/popper.min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/owl.carousel.min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/wow.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/jquery.nav.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/scrolling-nav.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/jquery.easing.min.js"></script>  
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/main.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/form-validator.min.js"></script>
+    <script src="<?php echo home_url(); ?>/app/themes/dbpmarketplace/assets/js/contact-form-script.min.js"></script>
       
   </body>
 </html>
